0fc58ca10a3295d5d9da6a093dacdd45078d9f29
yuriy <juriy9@yandex.ru>
2024-04-18 16:43:13 +0300

    wip

--- back/src/config.example.ts
+++ back/src/config.example.ts
@@ -1,21 +1,8 @@
 import { ConfigType } from './types/config.type.js';
 
 export const config: ConfigType = {
-    autoSendScreenshots: false,
-    moreCadnumsLoadCount: 10,
-    countCadnumsBeforeLoadMore: 3,
-    countDownloadedBeforeSendToMainServer: 5,
-    cadnumsWhereRegex: null,
-    // cadnumsWhereRegex: '^77:01:',
-    waitForWssClient: false,
     viewportParams: {
         width: 800,
         height: 2200,
     },
-    fullRestartIntervalMinutes: 60,
-    waitBetweenCadnumsSecs: 10,
-    attemptTimeoutSecs: 30,
-    waitBetweenAttemptsSecs: 5,
-    maxAttemptsBeforeFailure: 15,
-    onFailureWaitMinutes: 10,
 };
--- back/src/config.ts
+++ back/src/config.ts
@@ -1,21 +1,8 @@
 import { ConfigType } from './types/config.type.js';
 
 export const config: ConfigType = {
-    autoSendScreenshots: true,
-    moreCadnumsLoadCount: 10,
-    countCadnumsBeforeLoadMore: 3,
-    countDownloadedBeforeSendToMainServer: 1,
-    cadnumsWhereRegex: null,
-    // cadnumsWhereRegex: '^77:02:',
-    waitForWssClient: false,
     viewportParams: {
         width: 800,
         height: 800,
     },
-    fullRestartIntervalMinutes: 90,
-    waitBetweenCadnumsSecs: 10,
-    attemptTimeoutSecs: 60,
-    waitBetweenAttemptsSecs: 5,
-    maxAttemptsBeforeFailure: 15,
-    onFailureWaitMinutes: 5,
 };
