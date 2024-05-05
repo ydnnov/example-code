49208039724838203501562c2040184e5013c830
yuriy <juriy9@yandex.ru>
2024-05-05 14:48:32 +0300

    wip

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-      './src/components/**/*.{vue,html,js}',
-      './src/pages/**/*.{vue,html,js}',
+      './src/**/*.{vue,html,js}',
   ],
   theme: {
     extend: {},
