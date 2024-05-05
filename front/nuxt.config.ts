7b83ea9d039b37a71295728bf850bfa5b4d115c3
yuriy <juriy9@yandex.ru>
2024-05-05 14:48:53 +0300

    wip

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
     modules: [
         'nuxt-primevue',
         '@pinia/nuxt',
+        '@pinia-plugin-persistedstate/nuxt',
     ],
     primevue: {
         options: {
