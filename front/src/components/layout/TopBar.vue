e872d5a87c206175c8e3736372800e62d86e8e09
yuriy <juriy9@yandex.ru>
2024-06-05 14:25:38 +0300

    wip

--- front/src/components/layout/TopBar.vue
+++ front/src/components/layout/TopBar.vue
@@ -6,6 +6,7 @@ import { useUserStore } from '~/stores/user.store.js';
 import { useUiStore } from '~/stores/ui.store.js';
 import { useToast } from 'primevue/usetoast';
 import useClient from '~/composables/useClient.js';
+import TopBarLayoutControls from '~/components/layout/top-bar/TopBarLayoutControls.vue';
 
 const { bag } = useBagStore();
 const { user } = useUserStore();
@@ -71,31 +72,7 @@ const someAction = async () => {
       </template>
       <template #end>
         <div class="flex align-items-center gap-2">
-          <div class="">
-            <Button
-                @click="ui.mainSplitter.swapped = !ui.mainSplitter.swapped"
-                :icon="`pi pi-arrows-${ui.mainSplitter.horizontal ? 'h' : 'v'}`"
-                class="w-[40px] h-[40px] ml-4"
-            />
-            <Button
-                @click="ui.mainSplitter.horizontal = !ui.mainSplitter.horizontal"
-                icon="pi pi-desktop"
-                class="w-[40px] h-[40px] ml-2"
-                :class="ui.mainSplitter.horizontal ? '' : 'rotate-90'"
-            />
-          </div>
-          <div class="ml-2 border-l-[3px] border-black-500">
-            <Button
-                @click="ui.mainSplitter.panels[1] = 'code-exec'"
-                icon="pi pi-code"
-                class="w-[40px] h-[40px] ml-4"
-            />
-            <Button
-                @click="ui.mainSplitter.panels[1] = 'event-bus'"
-                icon="pi pi-align-left"
-                class="w-[40px] h-[40px] ml-2"
-            />
-          </div>
+          <TopBarLayoutControls />
           <div class="ml-2 border-l-[3px] border-black-500">
             <InputText
                 placeholder="Search"
---
+++ front/src/components/layout/top-bar/TopBarLayoutControls.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+import { useUiStore } from '~/stores/ui.store.js';
+
+const { ui } = useUiStore();
+</script>
+
+<template>
+  <div class="">
+    <Button
+        @click="ui.mainSplitter.swapped = !ui.mainSplitter.swapped"
+        :icon="`pi pi-arrows-${ui.mainSplitter.horizontal ? 'h' : 'v'}`"
+        class="w-[40px] h-[40px] ml-4"
+    />
+    <Button
+        @click="ui.mainSplitter.horizontal = !ui.mainSplitter.horizontal"
+        icon="pi pi-desktop"
+        class="w-[40px] h-[40px] ml-2"
+        :class="ui.mainSplitter.horizontal ? '' : 'rotate-90'"
+    />
+  </div>
+  <div class="ml-2 border-l-[3px] border-black-500">
+    <Button
+        @click="ui.mainSplitter.panels[1] = 'code-exec'"
+        icon="pi pi-code"
+        class="w-[40px] h-[40px] ml-4"
+    />
+    <Button
+        @click="ui.mainSplitter.panels[1] = 'event-bus'"
+        icon="pi pi-align-left"
+        class="w-[40px] h-[40px] ml-2"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
