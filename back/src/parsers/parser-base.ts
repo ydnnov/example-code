4b4acf41f0a81edb250b66251db9825ec19442fe
yuriy <juriy9@yandex.ru>
2024-06-16 13:32:48 +0300

    wip

--- back/src/parsers/parser-base.ts
+++ back/src/parsers/parser-base.ts
@@ -3,8 +3,11 @@ import { EmitsToBus } from '../classes/emits-to-bus.js';
 
 export abstract class ParserBase extends EmitsToBus {
 
-    // TODO здесь лучше задействовать шаблонный тип <T>
+    // TODO позднее, нужно будет задействовать шаблонный тип <T>
     //      вместо конерктного { resultHtml: string }
+    //      то есть вот так:
+    // public abstract run<TResultData>(): Promise<StdResult<TResultData>>;
+
     public abstract run(): Promise<StdResult<{ resultHtml: string }>>;
 
     public abstract extractJson(html: string);
--- back/src/services/services.ts
+++ back/src/services/services.ts
@@ -2,13 +2,10 @@ import { config } from '../config.js';
 import { CodeExecService } from './code-exec.service.js';
 import { EventBusService } from './event-bus.service.js';
 import { HeadlessService } from './headless.service.js';
-import { HeadlessServiceFactory } from '../factories/headless-service.factory.js';
 import { HeadlessScreenshotsService } from './headless-screenshots.service.js';
 import { ParserService } from './parser.service.js';
 import { SiteCaptchaService } from './site-captcha.service.js';
 import { TsDefinitionsService } from './ts-definitions.service.js';
-
-const headlessServiceFactory = new HeadlessServiceFactory();
 
 export const services = {
     bus: new EventBusService(),
