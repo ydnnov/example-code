38b07dbe7450af463d00dc5c4539b859a7391887
yuriy <juriy9@yandex.ru>
2024-06-18 19:14:25 +0300

    wip

--- back/src/entities/entities.ts
+++ back/src/entities/entities.ts
@@ -2,4 +2,3 @@ export { BusEventEntity } from './bus-event.entity.js';
 export { CaptchaAnswerRequestEntity } from './captcha-answer-request.entity.js';
 export { CaptchaImageEntity } from './captcha-image.entity.js';
 export { ParserTaskEntity } from './parser-task.entity.js';
-export { ParserEntity } from './parser.entity.js';
