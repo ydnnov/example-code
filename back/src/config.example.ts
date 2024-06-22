9acb4497b998620b027a3e955660c1e338f2219c
yuriy <juriy9@yandex.ru>
2024-06-22 19:32:24 +0300

    wip

--- back/src/config.example.ts
+++ back/src/config.example.ts
@@ -1,26 +1,17 @@
 import { ConfigType } from './types/config.type.js';
-import { PuppeteerLaunchOptions } from 'puppeteer';
 import { LaunchOptions as PlaywrightLaunchOptions } from 'playwright';
 
-const puppeteerLaunchOptions: PuppeteerLaunchOptions = {
-    headless: false,
-    args: ['--no-sandbox'],
-    ignoreHTTPSErrors: true,
-    defaultViewport: {
-        width: 800,
-        height: 1200,
-    },
-};
-
 const playwrightLaunchOptions: PlaywrightLaunchOptions = {
-    headless: false,
+    headless: true,
 };
 
 export const config: ConfigType = {
+    autoSendScreenshots: true,
     browserParams: {
-        type: 'puppeteer',
-        launchOptions: puppeteerLaunchOptions,
-        // type: 'playwright',
-        // launchOptions: playwrightLaunchOptions,
+        launchOptions: playwrightLaunchOptions,
+        viewportSize: {
+            width: 800,
+            height: 800,
+        },
     },
 };
