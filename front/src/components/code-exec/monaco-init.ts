1131779be63adc8e192519cbbc4a3d425d5bf164
yuriy <juriy9@yandex.ru>
2024-07-01 17:42:11 +0300

    wip

--- front/src/components/code-exec/monaco-init.ts
+++ front/src/components/code-exec/monaco-init.ts
@@ -1,10 +1,8 @@
 import * as monaco from 'monaco-editor';
 
-export const monacoInit = {
+export class MonacoInit {
   firstInit() {
-      console.log('monacoFirstInit');
-      if (!editorEl.value) return;
-      ////////////////////////////////////////////////////////////////////////////////
+    console.log('MonacoInit::firstInit');
 //   monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
 //     target: monaco.languages.typescript.ScriptTarget.ES2016,
 //     allowNonTsExtensions: true,
@@ -24,68 +22,69 @@ export const monacoInit = {
 //     noSyntaxValidation: false,
 //   });
 //
-      // const tsDefinitions = await client.tsDefinitions.all();
-      //
-      // monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-      //   target: monaco.languages.typescript.ScriptTarget.ES2016,
-      //   allowNonTsExtensions: true,
-      //   moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-      //   module: monaco.languages.typescript.ModuleKind.CommonJS,
-      //   noEmit: true,
-      //   typeRoots: ['node_modules/@types'],
-      // });
-      //
-      // monaco.languages.typescript.typescriptDefaults.addExtraLib(
-      //   tsDefinitions,
-      //   'file:///node_modules/@types/types/index.d.ts',
-      // );
-      //
-      // monaco.languages.typescript.typescriptDefaults.addExtraLib(
-      //     tsDefinitions,
-      //     'file:///node_modules/@types/playwright/index.d.ts',
-      // );
-      let code = getCurrentTab()?.code;
-      if (!code || !code.length) {
-        code = `import * as playwright from 'types';
+    // const tsDefinitions = await client.tsDefinitions.all();
+    //
+    // monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+    //   target: monaco.languages.typescript.ScriptTarget.ES2016,
+    //   allowNonTsExtensions: true,
+    //   moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+    //   module: monaco.languages.typescript.ModuleKind.CommonJS,
+    //   noEmit: true,
+    //   typeRoots: ['node_modules/@types'],
+    // });
+    //
+    // monaco.languages.typescript.typescriptDefaults.addExtraLib(
+    //   tsDefinitions,
+    //   'file:///node_modules/@types/types/index.d.ts',
+    // );
+    //
+    // monaco.languages.typescript.typescriptDefaults.addExtraLib(
+    //     tsDefinitions,
+    //     'file:///node_modules/@types/playwright/index.d.ts',
+    // );
+    let code = getCurrentTab()?.code;
+    if (!code || !code.length) {
+      code = `import * as playwright from 'types';
 import { Browser, Page } from 'types';
 
 declare const page: Page, browser: Browser;
 
 `;
+    }
+
+    const model = monaco.editor.createModel(
+      code,
+      'typescript',
+      monaco.Uri.parse('file:///main.tsx'),
+    );
+    ////////////////////////////////////////////////////////////////////////////////
+
+    editor = monaco.editor.create(editorEl.value, {
+      // value: getCurrentTab()?.code,
+      fontSize: 18,
+      language: 'typescript',
+      automaticLayout: true,
+      model,
+    });
+
+    editor.focus();
+
+    editor.getModel()?.onDidChangeContent((e) => {
+      const tab = getCurrentTab();
+      if (tab) {
+        tab.code = editor.getValue();
       }
+    });
 
-      const model = monaco.editor.createModel(
-        code,
-        'typescript',
-        monaco.Uri.parse('file:///main.tsx'),
-      );
-      ////////////////////////////////////////////////////////////////////////////////
+    editor.onKeyDown(e => {
+      if (e.ctrlKey && e.code === 'Enter') {
+        codeExecSend();
+        e.stopPropagation();
+      }
+    });
+  }
 
-      editor = monaco.editor.create(editorEl.value, {
-        // value: getCurrentTab()?.code,
-        fontSize: 18,
-        language: 'typescript',
-        automaticLayout: true,
-        model,
-      });
-
-      editor.focus();
-
-      editor.getModel()?.onDidChangeContent((e) => {
-        const tab = getCurrentTab();
-        if (tab) {
-          tab.code = editor.getValue();
-        }
-      });
-
-      editor.onKeyDown(e => {
-        if (e.ctrlKey && e.code === 'Enter') {
-          codeExecSend();
-          e.stopPropagation();
-        }
-      });
-  },
   reInit() {
 
-  },
-};
+  }
+}
